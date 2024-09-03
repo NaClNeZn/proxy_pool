f2e788b50eb63af7a217148cca491f3fc6741647
NaCl <1375186232@qq.com>
2024-09-03 11:18:15 +0800

    feat(proxy_pool): 🛠️ 更新脚本

---
+++ MyReadMe.md
@@ -0,0 +1,15 @@
+
+```shell
+mkdir -p /home/docker/proxy_pool
+touch /home/docker/proxy_pool/proxy.txt
+chmod 777 /home/docker/proxy_pool
+```
+
+```shell
+docker run -d \
+  --name proxy_pool \
+  -p 5010:5010 \
+  -v /home/docker/proxy_pool/proxy.txt:/app/proxy.txt \
+  -e DB_CONN=redis://:root@192.168.0.234:6379/0 \
+  registry.cn-hangzhou.aliyuncs.com/nacl-public/proxy_pool:v1.0
+```
--- bilibili_proxy.py
+++ bilibili_proxy.py
@@ -1,62 +1,64 @@
+import random
 import requests
 import time
+import fake_useragent
 
-# 通过代理池项目刷播放量，如果没有部署此项目，可移步bilibili_proxy.py
+# 实例化fake_useragent对象
+ua = fake_useragent.UserAgent()
 
-# 视频bv号放这里，格式如下
-
-bvid = ["BV1Q8ndepE36"]
+# 视频bv号
+bvid = ["BV1ZCHYeMEQt"]
 
 # 代理池地址，项目可见https://github.com/jhao104/proxy_pool
-
-getproxy = "http://127.0.0.1:5010/get/"
-deleteproxy = "http://127.0.0.1:5010/delete/?proxy={}"
-proxynum = "http://127.0.0.1:5010/count"
-
+getproxy = "http://192.168.0.234:5010/get/"
+deleteproxy = "http://192.168.0.234:5010/delete/?proxy={}"
+proxynum = "http://192.168.0.234:5010/count"
+# 真正点击地址
 url = "http://api.bilibili.com/x/click-interface/click/web/h5"
-
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'Accept-Language': 'zh-CN,zh;q=0.9',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Origin': 'https://www.bilibili.com',
-    'Connection': 'keep-alive'
-}
-
-reqdata = []
-for bv in bvid:
-    stime = str(int(time.time()))
-
-    while True:
-        resp = requests.get("http://api.bilibili.com/x/web-interface/view?bvid={}".format(bv), headers=headers)
-        resp_json = resp.json()
-        if "data" in resp_json:
-            getdata = resp_json["data"]
-            break
-
-    data = {
-        'aid': getdata["aid"],
-        'cid': getdata["cid"],
-        "bvid": bv,
-        'part': '1',
-        'mid': getdata["owner"]["mid"],
-        'lv': '6',
-        "stime": stime,
-        'jsonp': 'jsonp',
-        'type': '3',
-        'sub_type': '0',
-        'title': getdata["title"]
-    }
-    reqdata.append(data)
 
 
 def run():
     num = 0
     while int(requests.get(proxynum).json().get("count")) != 0:
-
         resp = requests.get(getproxy).json().get("proxy")
-        for data in reqdata:
+        for bv in bvid:
+            headers = {
+                'User-Agent': ua.random,
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                'Accept-Language': 'zh-CN,zh;q=0.9',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Origin': 'https://www.bilibili.com',
+                'Connection': 'keep-alive'
+            }
+            stime = str(int(time.time()))
+
+            while True:
+                try:
+                    resp_view = requests.get("http://api.bilibili.com/x/web-interface/view?bvid={}".format(bv),
+                                             headers=headers)
+                    resp_view_json = resp_view.json()
+                    if "data" in resp_view_json:
+                        getdata = resp_view_json["data"]
+                        break
+                except requests.RequestException as e:
+                    sleep_time = random.randint(10, 15)
+                    print(f"获取的视频数据时出错: {bv},等待{sleep_time}秒后重试, {e}")
+                    # 等待一段时间后重试
+                    time.sleep(sleep_time)
+
+            data = {
+                'aid': getdata["aid"],
+                'cid': getdata["cid"],
+                "bvid": bv,
+                'part': '1',
+                'mid': getdata["owner"]["mid"],
+                'lv': '6',
+                "stime": stime,
+                'jsonp': 'jsonp',
+                'type': '3',
+                'sub_type': '0',
+                'title': getdata["title"]
+            }
 
             try:
                 stime = str(int(time.time()))
@@ -67,17 +69,23 @@ def run():
                     "http": "http://{}".format(resp)
                 }
 
-                requests.post(url, headers=headers, data=data, proxies=proxy, timeout=5)
-
+                response = requests.post(url, headers=headers, data=data, proxies=proxy, timeout=5)
+                response.raise_for_status()  # 检查响应状态码
+                response.close()
+                num += 1
+                sleep_time = random.randint(10, 15)
+                print(f"done   {num},等待 {sleep_time} 秒后执行下一次")
+                time.sleep(sleep_time)
+            except requests.RequestException as e:
+                print(f"代理连接超时或请求错误: {e}")
             except Exception as e:
-                print("代理连接超时")
-
+                print(f"意外错误: {e}")
         requests.get(deleteproxy.format(resp))
-        num += 1
-        print("done   {}".format(num))
-
     print("无可用代理")
 
+
+if __name__ == '__main__':
+    run()
 
 if __name__ == '__main__':
     run()
